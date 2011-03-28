012695d6c67259e05a92aeffc8736084c1875b6d
Tj Holowaychuk <tj@vision-media.ca>
2011-03-28 12:33:59 -0700

    Added example

---
+++ example.js
@@ -0,0 +1,24 @@
+
+/**
+ * Module dependencies.
+ */
+
+var http = require('http')
+  , cluster = require('cluster')
+  , live = require('./');
+
+var server = http.createServer(function(req, res){
+  res.end('Hello World');
+});
+
+// localhost:3000 is our server
+// localhost:8888 (by default) is cluster-live 
+
+cluster(server)
+  .set('workers', 2)
+  .use(cluster.debug())
+  .use(cluster.repl(9999))
+  .use(cluster.stats({ connections: true, requests: true }))
+  .use(live())
+  .listen(3000);
+
